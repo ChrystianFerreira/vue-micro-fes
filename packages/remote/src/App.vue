f66da33937484dda95eb5af30ea72ab779e85795
Chrystian Ferreira <chrystian.profissional@gmail.com>
2022-04-25 21:00:46 -0300

    Vue Micro Front-ends

---
+++ packages/remote/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="bg-emerald-50 wrapper mt-10 text-3xl mx-auto max-w-6xl">
+    <Header />
+    <div>Name: remote</div>
+    <div>Framework: vue3</div>
+    <div>Language: JavaScript</div>
+    <div>CSS: Tailwind</div>
+  </div>
+</template>
+
+<script>
+import Header from "./Header.vue";
+
+export default {
+  components: {
+    Header
+  }
+}
+</script>
+
+<style scoped>
+.vue-remote-color {
+  background-color: #00C851;
+}
+
+.wrapper {
+  height: 100vh;
+  width: 100vh;
+  margin: 0;
+}
+</style>
---
+++ packages/remote/src/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div>
+      <h1>Header component</h1>
+      <p>@ Remote Vue App</p>
+    </div>
+
+
+    <div>
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+    </div>
+
+    <div>{{ count }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    count() {
+      return this.$store?.state?.count ?? 0;
+    }
+  }
+}
+</script>
